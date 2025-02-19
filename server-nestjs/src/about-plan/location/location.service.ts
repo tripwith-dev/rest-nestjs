8efda4b4c4746f78873fb8a96f35203070493b1f
1Dohyeon <dh1072005@gmail.com>
2025-02-19 20:09:38 +0900

    feat: location service CR

--- server-nestjs/src/about-plan/location/location.service.ts
+++ server-nestjs/src/about-plan/location/location.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LocationEntity } from './location.entity';
+import { LocationRepository } from './location.repository';
 
 @Injectable()
-export class LocationService {}
+export class LocationService {
+  constructor(private readonly locationRepository: LocationRepository) {}
+
+  async createLocation(
+    locationName: string,
+    latitude: string,
+    longitude: string,
+  ): Promise<LocationEntity> {
+    const location = await this.findLocationByName(locationName);
+
+    // location name 똑같으면 있는거 return, 없으면 새로 만듦
+    if (!location) {
+      return await this.locationRepository.createLocation(
+        locationName,
+        latitude,
+        longitude,
+      );
+    } else {
+      return location;
+    }
+  }
+
+  async findLocationById(locationId: number): Promise<LocationEntity> {
+    const location = await this.locationRepository.findLocationById(locationId);
+    if (!location) throw new NotFoundException("can't find location");
+    return location;
+  }
+
+  async findLocationByName(locationName: string): Promise<LocationEntity> {
+    const location =
+      await this.locationRepository.findLocationByName(locationName);
+    if (!location) throw new NotFoundException("can't find location");
+    return location;
+  }
+}
