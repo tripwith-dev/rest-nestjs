d297cbfb3bbfe1b1d0452baf530935c6270cba87
1Dohyeon <dh1072005@gmail.com>
2025-02-26 11:20:29 +0900

    refactor: find category

--- api/src/about-user/avatar/avatar.repository.ts
+++ api/src/about-user/avatar/avatar.repository.ts
@@ -16,30 +16,6 @@ export class AvatarRepository {
     private readonly repository: Repository<AvatarEntity>,
   ) {}
 
-  /** <주의>
-   * 모든 사용자에 대한 프로필 가져옴
-   */
-  async findAllAvatars(): Promise<AvatarEntity[]> {
-    return await this.repository.find({ where: { isDeleted: false } });
-  }
-
-  /**
-   * 사용자 페이지에서 사용됨.
-   * 프로필(아바타)에 대한 모든 정보를 반환함
-   */
-  async findAvatarWithCategoryByAvatarId(
-    avatarId: number,
-  ): Promise<AvatarEntity> {
-    const avatar = await this.repository
-      .createQueryBuilder('avatar')
-      .leftJoinAndSelect('avatar.categories', 'category')
-      .where('avatar.avatarId = :avatarId', { avatarId })
-      .andWhere('avatar.isDeleted = false')
-      .getOne();
-
-    return avatar;
-  }
-
   /**
    * 프로필 정보만 조회
    */
@@ -53,9 +29,25 @@ export class AvatarRepository {
     return avatar;
   }
 
-  async findAvatarWithLikePlansByAvatarId(
-    avatarId: number,
-  ): Promise<AvatarEntity> {
+  /**
+   * 사용자가 좋아요를 누른 플랜을 제외한 사용자가 생성한
+   * 카테고리, 플랜, 게시글, 댓글을 반환함
+   */
+  async findAvatarDetailById(avatarId: number): Promise<AvatarEntity> {
+    const avatar = await this.repository
+      .createQueryBuilder('avatar')
+      .leftJoinAndSelect('avatar.categories', 'category')
+      .leftJoinAndSelect('avatar.plans', 'plan')
+      .leftJoinAndSelect('avatar.articles', 'article')
+      .leftJoinAndSelect('avatar.articleComments', 'articleComment')
+      .where('avatar.avatarId = :avatarId', { avatarId })
+      .andWhere('avatar.isDeleted = false')
+      .getOne();
+
+    return avatar;
+  }
+
+  async findAvatarLikePlansByAvatarId(avatarId: number): Promise<AvatarEntity> {
     const avatar = await this.repository
       .createQueryBuilder('avatar')
       .leftJoinAndSelect('avatar.likePlans', 'likePlans')
