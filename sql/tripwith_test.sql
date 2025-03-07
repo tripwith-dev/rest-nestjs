cc962f4641b77293036ad470c5805c65fa7e56b4
Dohyeon Won <dh1072005@gmail.com>
2025-03-07 21:21:03 +0900

    feat: test table select sql

--- sql/tripwith_test.sql
+++ sql/tripwith_test.sql
@@ -13,3 +13,26 @@ select * from tripwith_test.location_type_mapping;
 
 SET SQL_SAFE_UPDATES = 0;
 update tripwith.plan_detail set price=null;
+
+-- USE tripwith
+-- USE tripwith_test
+-- SHOW TABLES;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE article;
+TRUNCATE TABLE article_comment;
+TRUNCATE TABLE avatar;
+TRUNCATE TABLE avatar_like_plan;
+TRUNCATE TABLE category;
+TRUNCATE TABLE location;
+TRUNCATE TABLE location_type;
+TRUNCATE TABLE location_type_mapping;
+TRUNCATE TABLE plan;
+TRUNCATE TABLE plan_comment;
+TRUNCATE TABLE plan_detail;
+TRUNCATE TABLE plan_tag;
+TRUNCATE TABLE plan_tag_mapping;
+TRUNCATE TABLE user;
+
+SET FOREIGN_KEY_CHECKS = 1;
